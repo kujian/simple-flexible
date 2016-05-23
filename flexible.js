809ba1e12f5d78b0404b029ea0653194a2a1978e
kujian <cbaojian@gmail.com>
2016-05-23 09:00:37 +0800

    Update flexible.js

--- flexible.js
+++ flexible.js
@@ -2,8 +2,8 @@
 //website:http://caibaojian.com
 //weibo:http:weibo.com/kujian
 //兼容UC竖屏转横屏出现的BUG
-自定义设计稿的宽度：designWidth
-最大宽度:maxWidth
+//自定义设计稿的宽度：designWidth
+//最大宽度:maxWidth
 ;(function(designWidth, maxWidth) {
 	var doc = document,
 		win = window;
@@ -20,7 +20,6 @@
 			width = maxWidth;
 		}
 		var rem = width * 100 / designWidth;
-		//兼容UC开始
 		rootStyle="html{font-size:"+rem+'px !important}';
 		rootItem = document.getElementById('rootsize') || document.createElement("style");
 		if(!document.getElementById('rootsize')){
@@ -32,18 +31,17 @@
 		}else{
 		try{rootItem.innerHTML=rootStyle}catch(f){rootItem.innerText=rootStyle}
 		}
-		//兼容UC结束
 		docEl.style.fontSize = rem + "px";
 	};
 	refreshRem();
 
 	win.addEventListener("resize", function() {
-		clearTimeout(tid); //·ÀÖ¹Ö´ÐÐÁ½´Î
+		clearTimeout(tid); //防止执行两次
 		tid = setTimeout(refreshRem, 300);
 	}, false);
 
 	win.addEventListener("pageshow", function(e) {
-		if (e.persisted) { // ä¯ÀÀÆ÷ºóÍËµÄÊ±ºòÖØÐÂ¼ÆËã
+		if (e.persisted) { // 浏览器后退的时候重新计算
 			clearTimeout(tid);
 			tid = setTimeout(refreshRem, 300);
 		}
@@ -54,6 +52,6 @@
 	} else {
 		doc.addEventListener("DOMContentLoaded", function(e) {
 			doc.body.style.fontSize = "16px";
-		}, false)
+		}, false);
 	}
 })(640, 640);
