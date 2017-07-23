dd93e7113173cb7927623a281f034c2277760c1a
蔡宝坚 <cbaojian@gmail.com>
2017-07-23 11:29:41 +0800

    Update flexible.js

--- flexible.js
+++ flexible.js
@@ -4,6 +4,7 @@
 //兼容UC竖屏转横屏出现的BUG
 //自定义设计稿的宽度：designWidth
 //最大宽度:maxWidth
+//这段js的最后面有两个参数记得要设置，一个为设计稿实际宽度，一个为制作稿最大宽度，例如设计稿为750，最大宽度为750，则为(750,750)
 ;(function(designWidth, maxWidth) {
 	var doc = document,
 	win = window,
@@ -49,4 +50,4 @@
 			doc.body.style.fontSize = "16px";
 		}, false);
 	}
-})(640, 640);
+})(750, 750);
